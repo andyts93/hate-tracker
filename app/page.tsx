4ef671f9061f93f4c6b5dbbbd281314fb2f45c37
Andrea Castellini <and1@live.it>
2024-09-20 16:36:17 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import { Slider } from "@nextui-org/slider";
+import { TiHeartFullOutline } from "react-icons/ti";
+import { FaRegAngry } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen px-4">
+      <div className="flex flex-col">
+        <h1 className="text-3xl font-bold text-center uppercase bg-gradient-to-br from-purple-500 to-red-500 bg-clip-text text-transparent">You think you hate someone? Well, good for you! Track it here.</h1>
+        <Slider
+          className="max-w-md mt-6"
+          color="warning"
+          defaultValue={0}
+          endContent={<FaRegAngry className="text-2xl text-orange-400" />}
+          fillOffset={0}
+          formatOptions={{ signDisplay: "always" }}
+          label="Hate level"
+          maxValue={10}
+          minValue={-10}
+          size="lg"
+          startContent={
+            <TiHeartFullOutline className="text-2xl text-red-500" />
+          }
+          step={0.5}
+        />
+        <button className="px-4 py-1 rounded-2xl bg-green-500 mt-4 mx-24">Save</button>
+      </div>
+    </div>
+  );
+}
