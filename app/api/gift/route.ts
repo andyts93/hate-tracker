0c0b3624baf34302bb8e8988f60b4ac01f4565a1
Andrea <app@shock-wave.it>
2024-11-08 15:08:31 +0100

    fix

--- app/api/gift/route.ts
+++ app/api/gift/route.ts
@@ -1,5 +1,6 @@
-import { sql } from "@/sql";
 import { NextRequest, NextResponse } from "next/server";
+
+import { sql } from "@/sql";
 
 export async function GET(request: NextRequest) {
   const rows = await sql`SELECT * FROM gifts`;
